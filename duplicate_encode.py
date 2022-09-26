fe68af418ffc7d218b8899707511643d0af8e468
bradleythomas <andythomas.py@gmail.com>
2022-09-26 15:56:05 -0500

    Add 3and5

--- duplicate_encode.py
+++ duplicate_encode.py
@@ -48,3 +48,8 @@ def convert_to_lower(char):
 
 
 print(duplicate_encode("Tatter"))
+
+# I begin each day with an hour of practice using datastructures and building algorithms. The files attached are
+# challenges from Codewars.com. I take a different with each file and note the intention at the beginning in the
+# comments. Sometimes the intention is to use the least number of lines, other times it might be to write everything by
+# hand and not using pre-built methods.
